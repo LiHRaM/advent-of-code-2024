33381328598632540b0ae9af7f14ccf020d28437
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2024-12-04 07:47:14 +0100

    feat(template): simplify process definition

--- src/template.zig
+++ src/template.zig
@@ -6,8 +6,8 @@ const testing = std.testing;
 const DATA = @embedFile("day-{{day}}-1.txt");
 const EXAMPLE_DATA = @embedFile("day-{{day}}-1.example.txt");
 
-fn process(comptime n: usize, data: *const [n:8]u8) !usize {
-    _ = data;
+fn process(comptime text: []const u8) !usize {
+    _ = text;
 
     return 0;
 }
