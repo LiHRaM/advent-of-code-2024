46c48a5c59bf6811abb7c021d6f13e7ff46c650f
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2024-12-07 23:19:13 +0100

    feat: day 5 pt 2

---
+++ src/day-5-2.zig
@@ -0,0 +1,132 @@
+//! Solution for: https://adventofcode.com/2024/day/5
+
+const std = @import("std");
+const testing = std.testing;
+const DATA = @embedFile("day-5-1.txt");
+const EXAMPLE_DATA = @embedFile("day-5-1.example.txt");
+
+fn Rule(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        before: T,
+        after: T,
+    };
+}
+
+const Cmp = []const u8;
+
+const Rules = std.MultiArrayList(Rule(Cmp));
+const BanList = std.ArrayList(Cmp);
+
+fn SortByRules(rules: Rules, a: Cmp, b: Cmp) bool {
+    for (rules.items(.before), rules.items(.after)) |before, after| {
+        if (std.mem.eql(u8, before, a) and std.mem.eql(u8, after, b)) {
+            return true;
+        } else if (std.mem.eql(u8, before, b) and std.mem.eql(u8, after, a)) {
+            return false;
+        }
+    }
+    return false;
+}
+
+const State = enum {
+    Rules,
+    Updates,
+};
+
+const RuleParseError = error{
+    InvalidSize,
+    InvalidDelimiter,
+};
+
+fn parseRule(text: Cmp) RuleParseError!Rule(Cmp) {
+    if (text.len != 5) return RuleParseError.InvalidSize;
+    if (text[2] != '|') return RuleParseError.InvalidDelimiter;
+
+    return .{
+        .before = text[0..2],
+        .after = text[3..5],
+    };
+}
+
+test "parseRule" {
+    try std.testing.expectEqualDeep(Rule(Cmp){ .before = "01", .after = "02" }, parseRule("01|02"));
+    try std.testing.expectEqualDeep(Rule(Cmp){ .before = "02", .after = "01" }, parseRule("02|01"));
+    try std.testing.expectEqualDeep(RuleParseError.InvalidSize, parseRule("0110"));
+    try std.testing.expectEqualDeep(RuleParseError.InvalidDelimiter, parseRule("01010"));
+}
+
+fn process(comptime text: []const u8) !usize {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var rules = Rules{};
+    defer rules.deinit(allocator);
+
+    var state: State = .Rules;
+    var lines = std.mem.splitScalar(u8, text, '\n');
+
+    var sum: usize = 0;
+
+    while (lines.next()) |line| {
+        if (line.len == 0) {
+            if (state == .Rules) {
+                state = .Updates;
+            }
+            continue;
+        }
+        switch (state) {
+            .Rules => {
+                errdefer |err| std.log.err("Processing rule failed: {}\n", .{err});
+                const rule = try parseRule(line);
+                try rules.append(allocator, rule);
+            },
+            .Updates => {
+                var hasBadOrder: bool = false;
+
+                var numbers = std.ArrayList(Cmp).init(allocator);
+                defer numbers.deinit();
+
+                var bans = std.StringHashMap(void).init(allocator);
+                defer bans.deinit();
+
+                var updateIter = std.mem.splitScalar(u8, line, ',');
+                while (updateIter.next()) |num| {
+                    if (bans.get(num)) |_| {
+                        hasBadOrder = true;
+                    }
+
+                    try numbers.append(num);
+
+                    if (hasBadOrder) continue;
+
+                    for (rules.items(.before), rules.items(.after)) |before, after| {
+                        if (std.mem.eql(u8, num, after)) {
+                            try bans.put(before, undefined);
+                        }
+                    }
+                }
+
+                if (!hasBadOrder) continue;
+
+                std.mem.sort(Cmp, numbers.items, rules, SortByRules);
+
+                const middlePos = @divFloor(numbers.items.len, 2);
+                const middleNumber = try std.fmt.parseInt(usize, numbers.items[middlePos], 10);
+                sum += middleNumber;
+            },
+        }
+    }
+
+    return sum;
+}
+
+pub fn main() !void {
+    const result = try process(DATA);
+    std.debug.print("Result: {d}\n", .{result});
+}
+
+test "example" {
+    try std.testing.expectEqual(123, process(EXAMPLE_DATA));
+}
